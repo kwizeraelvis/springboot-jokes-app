1482b2f177d5ff070c1687139d82c3d52c96785a
Kwizera Aime Elvis <kwizeraelvis6@gmail.com>
2020-01-16 22:24:22 +0200

    Completed building spring chuck norris joke app

---
+++ src/main/java/com/elvis/springapp/springbootjokesapp/service/JokeService.java
@@ -0,0 +1,5 @@
+package com.elvis.springapp.springbootjokesapp.service;
+
+public interface JokeService {
+    String getJoke();
+}
---
+++ src/test/java/com/elvis/springapp/springbootjokesapp/SpringbootJokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elvis.springapp.springbootjokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJokesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
