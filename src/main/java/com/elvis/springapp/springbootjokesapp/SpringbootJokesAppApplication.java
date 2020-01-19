5537f3a92cfc1f16ac7fd34c59ed6d3c6718f046
Kwizera Aime Elvis <kwizeraelvis6@gmail.com>
2020-01-20 01:23:04 +0200

    Xml based Configuration Stereotype

--- src/main/java/com/elvis/springapp/springbootjokesapp/SpringbootJokesAppApplication.java
+++ src/main/java/com/elvis/springapp/springbootjokesapp/SpringbootJokesAppApplication.java
@@ -2,8 +2,10 @@ package com.elvis.springapp.springbootjokesapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class SpringbootJokesAppApplication {
 
     public static void main(String[] args) {
